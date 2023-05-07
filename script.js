845e155a7a88db147cc89173ec61fd079ea1a3e9
NADEEM  ABBAS <79992599+nadeemabbas24@users.noreply.github.com>
2023-05-07 22:53:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 // myCartProductArray = myProducts.filter((item)=> myCartIDs.includes(item.id))
 
 document.querySelector(".login-btn").addEventListener("click",(e)=>{
-    window.location.href = "/login";
+    window.location.href = "/f3shopping-cart/login";
 })
 document.querySelector(".signup-btn").addEventListener("click",(e)=>{
-    window.location.href = "/signup";
+    window.location.href = "/f3shopping-cart/signup";
 })
